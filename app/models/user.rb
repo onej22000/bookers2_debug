7235c8d6a5228909ad3bafac21f26341280dcc7a
EC2 Default User <ec2-user@ip-172-31-0-179.ec2.internal>
2022-02-04 05:59:02 +0000

    フォロー人数表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,10 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
